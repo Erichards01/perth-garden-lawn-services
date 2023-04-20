959ae919bbafe13c789a33a1afe0472c9db63fa4
Erichards01 <erichards066@gmail.com>
2023-04-20 19:08:37 +0800

    hero logo added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,43 @@
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
 import heroImg from "../public/Lawn3.jpg";
+import logoMower from "../public/logoMower.jpg";
+import logoLeaf from "../public/logoLeaf.jpg";
+import logoBin from "../public/logoBin.jpg";
 
 export default function Home() {
   return (
     <>
       <div className={styles.heroContainer}>
+        <Image
+          src={logoMower}
+          placeholder="blur"
+          alt=""
+          className={styles.content}
+          style={{ height: "100px", width: "100px", marginRight: "20px" }}
+        />
+        <Image
+          src={logoLeaf}
+          placeholder="blur"
+          alt=""
+          className={styles.content}
+          style={{ height: "100px", width: "100px", marginRight: "20px" }}
+        />
+        <Image
+          src={logoBin}
+          placeholder="blur"
+          alt=""
+          className={styles.content}
+          style={{ height: "100px", width: "100px", marginRight: "20px" }}
+        />
         <h1 className={styles.content} id={styles.heroHeader}>
           Perth Garden and Lawn Services
         </h1>{" "}
         <Image
           src={heroImg}
+          placeholder="blur"
           alt="lawn photograph"
           className={styles.backgroundImage}
-          placeholder="blur"
           style={{
             position: "absolute",
             margin: "0",

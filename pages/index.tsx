847d94bc60d098f1748e591cd8a5328ad637e2c2
Erichards01 <erichards066@gmail.com>
2023-04-20 18:03:14 +0800

    heroImage and Home.module.css added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,27 @@
-import Head from "next/head";
+import styles from "../styles/Home.module.css";
 import Image from "next/image";
-
-import styles from "@/styles/Home.module.css";
+import heroImg from "../public/Lawn3.jpg";
 
 export default function Home() {
   return (
     <>
-      <h1>Perth Garden and Lawn Services</h1>
+      <div className={styles.heroContainer}>
+        <h1 className={styles.content} id={styles.heroHeader}>
+          Perth Garden and Lawn Services
+        </h1>{" "}
+        <Image
+          src={heroImg}
+          alt="lawn photograph"
+          className={styles.backgroundImage}
+          placeholder="blur"
+          style={{
+            position: "absolute",
+            margin: "0",
+            padding: "0",
+            outline: "0",
+          }}
+        />
+      </div>
     </>
   );
 }

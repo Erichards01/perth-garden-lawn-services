6923da3304695b081de38a4dc7f00a92b60c45e1
Erichards01 <erichards066@gmail.com>
2023-04-23 13:49:47 +0800

    header and footer edited

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,7 @@
 import "@/styles/globals.css";
+import Image from "next/image";
+import headerImage from "../public/wood1.jpeg";
+import footerImage from "../public/LawnFooter.jpg";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
 import Head from "next/head";
@@ -9,48 +12,60 @@ const inter = Inter({ subsets: ["latin"] });
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
   return (
-    <div className={inter.className}>
-      <Head>
-        <title>Perth Garden and Lawn Services</title>
-        <meta name="description" content="Perth Garden and Lawn Services" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className="container">
+      <div className={inter.className}>
+        <Head>
+          <title>Perth Garden and Lawn Services</title>
+          <meta name="description" content="Perth Garden and Lawn Services" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <nav>
-        <ul>
-          <li>
-            <Link className={router.pathname == "/" ? "active" : ""} href="/">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              className={router.pathname == "/about" ? "active" : ""}
-              href="/about"
-            >
-              About
-            </Link>
-          </li>{" "}
-          <li>
-            <Link
-              className={router.pathname == "/services" ? "active" : ""}
-              href="/services"
-            >
-              Services
-            </Link>
-          </li>{" "}
-          <li>
-            <Link
-              className={router.pathname == "/contact" ? "active" : ""}
-              href="/contact"
-            >
-              Contact
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <Component {...pageProps} />
+        <nav>
+          <Image
+            src={headerImage}
+            alt=" dark vertical timber panels"
+            placeholder="blur"
+            className="headerImage"
+          />{" "}
+          <ul>
+            <li>
+              <Link className={router.pathname == "/" ? "active" : ""} href="/">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                className={router.pathname == "/about" ? "active" : ""}
+                href="/about"
+              >
+                About
+              </Link>
+            </li>{" "}
+            <li>
+              <Link
+                className={router.pathname == "/services" ? "active" : ""}
+                href="/services"
+              >
+                Services
+              </Link>
+            </li>{" "}
+            <li>
+              <Link
+                className={router.pathname == "/contact" ? "active" : ""}
+                href="/contact"
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <footer>
+          <Image src={footerImage} alt="grass" className="footerImage" />
+        </footer>
+
+        <Component {...pageProps} />
+      </div>
     </div>
   );
 }

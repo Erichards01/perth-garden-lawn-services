def01f500717a8f35cccccdc9ca8bec6f39402cf
Erichards01 <erichards066@gmail.com>
2023-04-24 14:49:59 +0800

    next config - fix build issue

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+// module.exports = nextConfig
+module.exports = (_phase, { defaultConfig }) => {
+  const plugins = [withStaticImport, withBundleAnalyzer, withCustomWebpack];
+  return plugins.reduce((acc, plugin) => plugin(acc), {
+    ...defaultConfig,
+    ...config,
+  });
+};

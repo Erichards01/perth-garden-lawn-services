c2ca06904384c0bea67d78279c9515699cb496b3
Erichards01 <erichards066@gmail.com>
2023-04-24 14:57:12 +0800

    plugin next config added

--- next.config.js
+++ next.config.js
@@ -3,11 +3,18 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-// module.exports = nextConfig
+module.exports = nextConfig;
+
+const plugins = [withTM];
+
 module.exports = (_phase, { defaultConfig }) => {
-  const plugins = [withStaticImport, withBundleAnalyzer, withCustomWebpack];
-  return plugins.reduce((acc, plugin) => plugin(acc), {
-    ...defaultConfig,
-    ...config,
-  });
+  return plugins.reduce(
+    (acc, plugin) => {
+      if (Array.isArray(plugin)) {
+        return plugin[0](acc, plugin[1]);
+      }
+      return plugin(acc);
+    },
+    { ...nextConfig }
+  );
 };

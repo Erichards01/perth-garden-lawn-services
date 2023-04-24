5cd882f97bf082c2fc44bb96897f30dee97fb0c7
Erichards01 <erichards066@gmail.com>
2023-04-24 15:00:01 +0800

    webpack issue

--- next.config.js
+++ next.config.js
@@ -1,20 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  future: { webpack5: true },
 };
 
 module.exports = nextConfig;
-
-const plugins = [withTM];
-
-module.exports = (_phase, { defaultConfig }) => {
-  return plugins.reduce(
-    (acc, plugin) => {
-      if (Array.isArray(plugin)) {
-        return plugin[0](acc, plugin[1]);
-      }
-      return plugin(acc);
-    },
-    { ...nextConfig }
-  );
-};

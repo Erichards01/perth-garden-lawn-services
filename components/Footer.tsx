49cc813eed77c52cc3b76e5b627f1e4f4829d254
Erichards01 <erichards066@gmail.com>
2023-05-01 17:33:55 +0800

    add logo to footer and fix up position bugs

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,19 @@
 import footerImage from "../public/LawnFooter.jpg";
 import React from "react";
 import Image from "next/image";
+import logo from "../public/LogoComplete.jpg";
 
 export default function Footer() {
   return (
     <div>
       <footer>
         <Image src={footerImage} alt="grass" className="footerImage" />
+        <Image
+          src={logo}
+          alt="lawn mowing logo"
+          placeholder="blur"
+          className="logo"
+        />
       </footer>
     </div>
   );

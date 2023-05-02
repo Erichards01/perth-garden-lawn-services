5a275b4c7b2402b47c3f4964029a82419557a55f
Erichards01 <erichards066@gmail.com>
2023-05-02 17:04:41 +0800

    nav added to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,8 +2,12 @@ import footerImage from "../public/LawnFooter.jpg";
 import React from "react";
 import Image from "next/image";
 import logo from "../public/LogoComplete.jpg";
+import Link from "next/link";
+import facebook from "../public/facebook.png";
+import { useRouter } from "next/router";
 
 export default function Footer() {
+  const router = useRouter();
   return (
     <div>
       <footer>
@@ -14,6 +18,46 @@ export default function Footer() {
           placeholder="blur"
           className="logo"
         />
+        <ul>
+          <li className="foot">
+            <Link
+              className={router.pathname == "/" ? "footerActive" : ""}
+              href="/"
+            >
+              Home
+            </Link>
+          </li>
+          <li className="foot">
+            <Link
+              className={router.pathname == "/about" ? "footerActive" : ""}
+              href="/about"
+            >
+              About
+            </Link>
+          </li>{" "}
+          <li className="foot">
+            <Link
+              className={router.pathname == "/services" ? "footerActive" : ""}
+              href="/services"
+            >
+              Services
+            </Link>
+          </li>{" "}
+          <li className="foot">
+            <Link
+              className={router.pathname == "/contact" ? "footerActive" : ""}
+              href="/contact"
+            >
+              Contact
+            </Link>
+          </li>{" "}
+          {/* <a
+            href="https://www.facebook.com/Perthgardenandlawn"
+            className="footerFacebook"
+          >
+            <Image src={facebook} alt="facebook" className="Facebook" />
+          </a> */}
+        </ul>
       </footer>
     </div>
   );
